8cd16b11c91457aad9cb0bee8ced68c74c173933
Maria Martínez Santolaria <73641718+Santopots@users.noreply.github.com>
2025-03-25 10:00:24 +0000

    api maria

--- app/backend/src/API/items.py
+++ app/backend/src/API/items.py
@@ -22,13 +22,13 @@ class ItemUpdate(BaseModel):
 async def get_items():
     return mock_items
 
-# @router.post("/")
-# async def create_item(item: Item):
-#   for existing_item in mock_items:
-#       if existing_item.id == item.id:
-#           raise HTTPException(status_code=400, detail="El ID ya existe")
-#   mock_items.append(item)
-#   return item
+@router.post("/")
+async def create_item(item: Item):
+    for existing_item in mock_items:
+        if existing_item.id == item.id:
+        raise HTTPException(status_code=400, detail="El ID ya existe")
+    mock_items.append(item)
+    return item
 
 #para crear un nuevo item
 
