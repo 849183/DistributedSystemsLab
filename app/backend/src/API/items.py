cec7af39773432e8339bc6b88111fa6139b326e8
Maria Martínez Santolaria <73641718+Santopots@users.noreply.github.com>
2025-03-31 06:59:09 +0000

    tests done - missing testing

--- app/backend/src/API/items.py
+++ app/backend/src/API/items.py
@@ -26,10 +26,9 @@ async def get_items():
 async def create_item(item: Item):
     for existing_item in mock_items:
         if existing_item.id == item.id:
-        raise HTTPException(status_code=400, detail="El ID ya existe")
+            raise HTTPException(status_code=400, detail="El ID ya existe")
     mock_items.append(item)
     return item
-
 #para crear un nuevo item
 
 
--- app/backend/src/tests/test_all.py
+++ app/backend/src/tests/test_all.py
@@ -38,3 +38,22 @@ def test_delete_existing_item():
 def test_delete_nonexistent_item():
     response = client.delete("/items/999")
     assert response.status_code == 404
+
+# test maria
+
+def test_get_items_empty():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == []
+
+def test_create_item_success():
+    item_data = {"name": "Monitor UltraWide", "quantity": 7}
+    response = client.post("/", json=item_data)
+    assert response.status_code == 200
+    assert response.json() == item_data
+
+def test_get_items_after_create():
+    response = client.get("/")
+    assert response.status_code == 200
+    expected_items = [{"id": 1, "name": "Test Item", "description": "This is a test item"}]
+    assert response.json() == expected_items
