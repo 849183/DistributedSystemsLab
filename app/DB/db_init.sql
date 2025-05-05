b00b914b976feecd15cbe8c0885ff6ec8f5006fa
849183 <128531189+849183@users.noreply.github.com>
2025-05-05 08:50:27 +0000

    db updates

--- app/DB/db_init.sql
+++ app/DB/db_init.sql
@@ -56,16 +56,7 @@ INSERT INTO public.item (name, quantity) VALUES ('Paper Clips', 300);
 INSERT INTO public.item (name, quantity) VALUES ('Calculator', 18);
 
 
-INSERT INTO public.item (name, quantity) VALUES ('Laptop', 15);
-INSERT INTO public.item (name, quantity) VALUES ('Keyboard', 45);
-INSERT INTO public.item (name, quantity) VALUES ('Mouse', 60);
-INSERT INTO public.item (name, quantity) VALUES ('Monitor', 20);
-INSERT INTO public.item (name, quantity) VALUES ('Desk Chair', 10);
-INSERT INTO public.item (name, quantity) VALUES ('Notebook', 100);
-INSERT INTO public.item (name, quantity) VALUES ('Pen', 200);
-INSERT INTO public.item (name, quantity) VALUES ('USB Drive', 80);
-INSERT INTO public.item (name, quantity) VALUES ('Webcam', 35);
-INSERT INTO public.item (name, quantity) VALUES ('Headphones', 40);
+
 
 
 --
